120abe5f24dea4b6def262934dcd1a849bc8e0ec
LodrikMtl <26026137+LodrikMtl@users.noreply.github.com>
2019-07-23 20:35:27 -0400

    Add a custom user model:
    -Add a unique email field
    -Add a bio field
    -Add it to admin page

--- lan_papascal/accounts/models.py
+++ lan_papascal/accounts/models.py
@@ -1,3 +1,10 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.translation import gettext as _
 
 # Create your models here.
+class User(AbstractUser):
+    #avatar = models.ImageField()
+    email = models.EmailField(unique=True)
+    bio = models.CharField(max_length=1000, default="")
+    

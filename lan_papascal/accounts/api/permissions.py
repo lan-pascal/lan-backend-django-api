c31067f5743937519a755c6e3bd8d4f08cd0a53e
LodrikMtl <26026137+LodrikMtl@users.noreply.github.com>
2019-07-23 20:38:24 -0400

    Remove unnecessary permission

--- lan_papascal/accounts/api/permissions.py
+++ lan_papascal/accounts/api/permissions.py
@@ -14,19 +14,3 @@ class IsUserOrReadOnly(permissions.BasePermission):
 
         # Instance must be the `user`.
         return obj == request.user
-
-class CanOnlyCreateOrDeleteOrUpdate(permissions.BasePermission):
-    """
-    Object-level permission to only allow owners of an object to edit it.
-    Assumes the model instance has an `owner` attribute.
-    """
-
-    def has_object_permission(self, request, view, obj):
-        # Read permissions are allowed to any request,
-        # so we'll always allow GET, HEAD or OPTIONS requests.
-        if request.method in permissions.SAFE_METHODS:
-            return False
-
-        return True
-
-

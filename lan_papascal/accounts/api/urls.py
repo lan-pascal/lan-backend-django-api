cba08be9396c51da1cc092a668a4ac8292edefa5
LodrikMtl <26026137+LodrikMtl@users.noreply.github.com>
2019-07-23 20:37:19 -0400

    Name paths in Accounts Urls

--- lan_papascal/accounts/api/urls.py
+++ lan_papascal/accounts/api/urls.py
@@ -4,12 +4,12 @@ from .views import UserList, UserDetails, SignIn, SignUp, RefreshToken, RevokeTo
 
 urlpatterns = [
     #Accounts API
-    path('users/', UserList.as_view()),
-    path('users/<username>/', UserDetails.as_view()),
+    path('users/', UserList.as_view(), name="users-list"),
+    path('users/<str:username>/', UserDetails.as_view(), name="users-details"),
 
     #Proxy Server/Authorization Server
-    path('signin/', SignIn.as_view()),
-    path('signup/', SignUp.as_view()),
-    path('refresh_token/', RefreshToken.as_view()),
-    path('revoke_token/', RevokeToken.as_view()),
+    path('signin/', SignIn.as_view(), name="sign-in"),
+    path('signup/', SignUp.as_view(), name="sign-up"),
+    path('refresh_token/', RefreshToken.as_view(), name="refresh-token"),
+    path('revoke_token/', RevokeToken.as_view(), name="revoke-token"),
 ]

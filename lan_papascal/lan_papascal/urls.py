c05fdadb6f64e09daa79a804a5d391c69a1bf94a
LodrikMtl <26026137+LodrikMtl@users.noreply.github.com>
2019-07-27 16:38:54 -0400

    Refactor url path to accounts app

--- lan_papascal/lan_papascal/urls.py
+++ lan_papascal/lan_papascal/urls.py
@@ -22,5 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
-    path('v1/accounts/',include('accounts.api.urls')),
+    path('accounts/',include('accounts.api.urls')),
 ]

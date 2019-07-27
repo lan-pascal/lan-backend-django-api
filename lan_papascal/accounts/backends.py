4645c57a217df6f159dc5f2e6b811dbb6c4673c5
LodrikMtl <26026137+LodrikMtl@users.noreply.github.com>
2019-07-27 16:44:18 -0400

    Small overall of AccountsBackend:
    -Refactor username -> identification
    -Exit early when credentials are empty
    -Remove unnecessary call to ModelBackend authenticate method

--- lan_papascal/accounts/backends.py
+++ lan_papascal/accounts/backends.py
@@ -1,18 +1,21 @@
 from django.contrib.auth import backends, get_user_model
 from django.db.models import Q
+
 User = get_user_model()
 
 
 class AccountsBackend(backends.ModelBackend):
 
-    def authenticate(self, request, username=None, password=None, **kwargs):
-        if username is None:
-            username = kwargs.get(UserModel.USERNAME_FIELD)
+    def authenticate(self, request, identification=None, password=None, **kwargs):
+        
+        if identification is None:
+            identification = kwargs.get(User.USERNAME_FIELD)
+        if identification is None or password is None:
+            return
         try:
-            user = User.objects.get(Q(username__iexact=username) | Q(email__iexact=username))
+            user = User.objects.get(Q(username__iexact=identification) | Q(email__iexact=identification))
         except User.DoesNotExist:
             User().set_password(password)
         else:
             if user.check_password(password) and self.user_can_authenticate(user):
                 return user
-        return super().authenticate(request, username, password, **kwargs)

96938f9b9e00498d6653234517ebc1fbf651b0ab
LodrikMtl <26026137+LodrikMtl@users.noreply.github.com>
2019-08-04 13:33:03 -0400

    Change AuthMethod from Enum to Flag

--- lan_papascal/registration/conf.py
+++ lan_papascal/registration/conf.py
@@ -1,13 +1,13 @@
-from enum import Enum
+from enum import Flag,auto
 
 from easysettings.app import AppSettings
 
 
-class AuthMethod(Enum):
-    USERNAME = 1
-    EMAIL = 2
-    USERNAME_EMAIL = 3
-
+class AuthMethod(Flag):
+    USERNAME = auto()
+    EMAIL = auto()
+    USERNAME_EMAIL = USERNAME & EMAIL
+    
 class Settings(AppSettings):
     #This setting is important to raise a properly error on serializer validation
     AUTH_BACKEND_METHOD = AuthMethod.USERNAME_EMAIL

2428b7783f913876be70716976f06475d628eabd
LodrikMtl <26026137+LodrikMtl@users.noreply.github.com>
2019-08-07 16:57:42 -0400

    Remove unnecessary settings

--- lan_papascal/registration/conf.py
+++ lan_papascal/registration/conf.py
@@ -1,15 +1,8 @@
 from enum import Flag,auto
 
 from easysettings.app import AppSettings
-
-
-class AuthMethod(Flag):
-    USERNAME = auto()
-    EMAIL = auto()
-    USERNAME_EMAIL = USERNAME & EMAIL
     
 class Settings(AppSettings):
-    #This setting is important to raise a properly error on serializer validation
-    AUTH_BACKEND_METHOD = AuthMethod.USERNAME_EMAIL
+    pass
 
 settings = Settings()

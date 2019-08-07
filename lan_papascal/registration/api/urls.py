105afff3e86d2d59378043f0717b60c634e3a7e8
LodrikMtl <26026137+LodrikMtl@users.noreply.github.com>
2019-08-07 10:49:37 -0400

    Update URLs from removed view

--- lan_papascal/registration/api/urls.py
+++ lan_papascal/registration/api/urls.py
@@ -1,13 +1,10 @@
 from django.urls import path, include
 
-from .views import SignInView, SignUpView, RefreshTokenView, RevokeTokenView, PasswordChangeView, PasswordResetView, PasswordChangeView, PasswordResetConfirmView
+from .views import RegisterView, PasswordChangeView, PasswordResetView, PasswordResetConfirmView
 
 
 urlpatterns = [
-    path('signin', SignInView.as_view(), name="sign-in"),
-    path('signup', SignUpView.as_view(), name="sign-up"),
-    path('refresh_token', RefreshTokenView.as_view(), name="refresh-token"),
-    path('revoke_token', RevokeTokenView.as_view(), name="revoke-token"),
+    path('register', RegisterView.as_view(), name="sign-up"),
     path('password/change', PasswordChangeView.as_view(), name="password-change"),
     path('password/reset', PasswordResetView.as_view(), name="password-reset"),
     path('password/reset/confirm', PasswordResetConfirmView.as_view(), name="password-reset-confirm")

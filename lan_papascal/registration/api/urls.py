8d2da49a3c4cf905f31d292cddbb5d3fd01c966d
LodrikMtl <26026137+LodrikMtl@users.noreply.github.com>
2019-08-07 20:12:04 -0400

    Rename urls
    -Rename register -> signup
    -Add slash to the end for consistency

--- lan_papascal/registration/api/urls.py
+++ lan_papascal/registration/api/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path, include
 
-from .views import RegisterView, PasswordChangeView, PasswordResetView, PasswordResetConfirmView
+from .views import SignUpView, PasswordChangeView, PasswordResetView, PasswordResetConfirmView
 
 
 urlpatterns = [
-    path('register', RegisterView.as_view(), name="sign-up"),
-    path('password/change', PasswordChangeView.as_view(), name="password-change"),
-    path('password/reset', PasswordResetView.as_view(), name="password-reset"),
-    path('password/reset/confirm', PasswordResetConfirmView.as_view(), name="password-reset-confirm")
+    path('signup/', SignUpView.as_view(), name="sign-up"),
+    path('password/change/', PasswordChangeView.as_view(), name="password-change"),
+    path('password/reset/', PasswordResetView.as_view(), name="password-reset"),
+    path('password/reset/confirm/', PasswordResetConfirmView.as_view(), name="password-reset-confirm")
 ]

a73543a88796fcecbe2fe38c6ec959cefe1d59bb
LodrikMtl <26026137+LodrikMtl@users.noreply.github.com>
2019-09-15 19:49:09 -0400

    Clean up useless api urls

--- lan_papascal/registration/api/urls.py
+++ lan_papascal/registration/api/urls.py
@@ -4,8 +4,4 @@ from .views import SignUpView, PasswordChangeView, PasswordResetView, PasswordRe
 
 
 urlpatterns = [
-    path('signup/', SignUpView.as_view(), name="sign_up"),
-    path('password/change/', PasswordChangeView.as_view(), name="password_change"),
-    path('password/reset/', PasswordResetView.as_view(), name="password_reset"),
-    path('password/reset/confirm/', PasswordResetConfirmView.as_view(), name="password_reset_confirm")
 ]

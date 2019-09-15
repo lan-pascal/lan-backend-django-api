5af24bfcd5b21bd46950f59b46cfccd3a9a60c8f
LodrikMtl <26026137+LodrikMtl@users.noreply.github.com>
2019-09-15 19:50:27 -0400

    Update urls to respect django convention

--- lan_papascal/registration/urls.py
+++ lan_papascal/registration/urls.py
@@ -11,14 +11,14 @@ urlpatterns = [
     path('signup/done', views.SignUpDoneView.as_view(),  name="signup_done"),
 
     path(
-        'password/reset/', 
+        'password_reset/', 
         django_views.PasswordResetView.as_view(
             email_template_name='registration/email/password_reset_body.html', 
             subject_template_name='registration/email/password_reset_subject.txt'
             ), 
         name='password_reset'
     ),
-    path('password/reset/done/', django_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
-    path('password/reset/confirm/<uidb64>/<token>/', django_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    path('password/reset/complete/', django_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    path('password_reset/done/', django_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('password_reset/confirm/<uidb64>/<token>/', django_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('password_reset/complete/', django_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]

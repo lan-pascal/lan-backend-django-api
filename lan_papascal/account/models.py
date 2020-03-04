46e8c8775f44a3f39e32bef28b986d033a375abc
LodrikMtl <karl-etienne.bolduc@outlook.com>
2020-03-04 16:13:57 -0500

    Update User model by removing unecessary stuff
    -This class should focus on authentication information
    
    ''Thoses informations should be move in another Model (e.g. Profile)''

--- lan_papascal/account/models.py
+++ lan_papascal/account/models.py
@@ -4,10 +4,5 @@ from django.utils.translation import gettext as _
 
 # Create your models here.
 class User(AbstractUser):
-    #avatar = models.ImageField()
     email = models.EmailField(unique=True)
-    bio = models.CharField(max_length=1000,blank=True)
-
-    is_public = models.BooleanField(default=True)
-
     

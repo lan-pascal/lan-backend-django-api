22e2f83de66ba1da9b6a3d175754f6487f7d2f31
LodrikMtl <karl-etienne.bolduc@outlook.com>
2020-03-04 14:31:28 -0500

    Change language en-us to fr-ca

--- lan_papascal/lan_papascal/settings/dev.py
+++ lan_papascal/lan_papascal/settings/dev.py
@@ -129,7 +129,7 @@ AUTH_USER_MODEL = 'account.User'
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fr-ca'
 
 TIME_ZONE = 'UTC'
 

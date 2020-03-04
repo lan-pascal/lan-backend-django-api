d45493d08202c77e015b04161ae99f8d7a43ddc5
LodrikMtl <karl-etienne.bolduc@outlook.com>
2020-03-04 16:27:52 -0500

    Remove permission accordingly to the User Model

--- lan_papascal/account/api/views.py
+++ lan_papascal/account/api/views.py
@@ -4,17 +4,16 @@ from rest_framework import serializers, generics, permissions
 from oauth2_provider.contrib.rest_framework import TokenHasReadWriteScope, TokenHasScope
 
 from .serializers import UserSerializer
-from .permissions import IsUserOrReadOnlyIfPublic
 
 User = get_user_model()
 
 class UserList(generics.ListAPIView):
-    permission_classes = [permissions.IsAdminUser & TokenHasReadWriteScope]
+    permission_classes = [permissions.IsAdminUser]
     queryset = User.objects.all()
     serializer_class = UserSerializer
     
 class UserDetails(generics.RetrieveUpdateAPIView):
-    permission_classes = [IsUserOrReadOnlyIfPublic & TokenHasReadWriteScope]
+    permission_classes = [TokenHasReadWriteScope]
     serializer_class = UserSerializer(partial=True)
     queryset = User.objects.all()
     lookup_field = "username"

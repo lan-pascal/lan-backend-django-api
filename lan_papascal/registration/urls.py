e3da4508c0dec4d9f4204f61c3ec46217891e73e
LodrikMtl <karl-etienne.bolduc@outlook.com>
2020-03-04 13:59:04 -0500

    Update template and urls
    -Move some templates to django_registration
    -Add the missing template for django_registration
    -Add social login for twitter in signin and registration_form
    -Minors fixies overall
    -Adapt urls to the new template name
    -Add django_registration urls

--- lan_papascal/registration/urls.py
+++ lan_papascal/registration/urls.py
@@ -1,15 +1,28 @@
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 from django.contrib.auth import views as django_views
+from django_registration.backends.activation import views as django_registration_views
+
 from . import views
+from . import forms
 
 urlpatterns = [
+    #Plugins
+    path(
+        "register/",
+        django_registration_views.RegistrationView.as_view(form_class=forms.RegistrationForm),
+        name="django_registration_register",
+    ),
+    path('',include('django_registration.backends.activation.urls')),
+    
     #Basic
-    path('signin/', views.SignInView.as_view(), name='signin'),
+    path('signin/', django_views.LoginView.as_view(template_name='registration/signin.html',redirect_authenticated_user=True), name='signin'),
     path('signout/', django_views.LogoutView.as_view(template_name='registration/signout.html'), name='signout'),
-    path('signup/', views.SignUpView.as_view(), name='signup'),
-    path('signup/done', views.SignUpDoneView.as_view(),  name="signup_done"),
-
+    
+    path('password_change/', django_views.PasswordChangeView.as_view(), name='password_change'),
+    path('password_change/done/', django_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+    
     path(
         'password_reset/', 
         django_views.PasswordResetView.as_view(
@@ -19,6 +32,6 @@ urlpatterns = [
         name='password_reset'
     ),
     path('password_reset/done/', django_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
-    path('password_reset/confirm/<uidb64>/<token>/', django_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    path('password_reset/complete/', django_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    path('reset/confirm/<uidb64>/<token>/', django_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/complete/', django_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
